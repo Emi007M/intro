d225efd36472ada368ed3170650a959c23652953
Emilia Ogluszka <Emilia.Ogluszka@snt.pl>
2023-10-16 15:01:02 +0200

    first intro

---
+++ IntroOne/Circle.cs
@@ -0,0 +1,26 @@
+﻿
+namespace IntroOne;
+internal static class Circle
+{
+    public static void CalculateField()
+    {
+        int r;
+        double field;
+
+        Console.WriteLine("Podaj wartość r");
+        r = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Pole koła o promieniu " + r);
+        field = Calculate(r);
+        Console.WriteLine(field);
+    }
+    public static double Calculate(int r)
+    {
+        double pi = Math.PI;
+        double r2 = Math.Pow(r, 2);
+
+        double x = r2 * pi;
+        return x;
+    }
+
+}
